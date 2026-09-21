e1be482464d859741a5c3ce2172cf65d5535fe9b
ThwiiipyMan <devannspidey555@outlook.com>
2026-09-21 21:24:38 +0800

    init: Initializing repository

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age = 20;
+    float gwa = 3.25;
+    double pi = 3.14159;
+    char rating = 'B';
+
+    printf("%d", age);
+    printf("\n%.2f", gwa);
+    printf("\n%lf", pi);
+    printf("\n%c", rating);
+    
+    return 0;
+}
