266e242dc21749c39861aa078384a0094664a1fd
ThwiiipyMan <devannspidey555@outlook.com>
2026-09-21 21:59:08 +0800

    feat: Completing second activity

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables{
+    public static void main(String[] args){
+        int age = 20;
+        double gwa = 3.25;
+        char grade = 'B';
+        boolean isMarried = false;
+        String name = "Devann";
+        double dblage = age;
+
+        System.out.println(dblage);
+        System.out.printf("%.2f\n", gwa);
+        System.out.println(grade);
+        System.out.println(isMarried);
+        System.out.println(name);
+        System.out.println(name + age + gwa);
+    }
+}
--- variables.c
+++ variables.c
@@ -5,11 +5,13 @@ int main(){
     float gwa = 3.25;
     double pi = 3.14159;
     char rating = 'B';
+    int x = 6.7;
 
     printf("%d", age);
     printf("\n%.2f", gwa);
     printf("\n%lf", pi);
     printf("\n%c", rating);
+    printf("\n%d", x);
     
     return 0;
 }
